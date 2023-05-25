f609d16d6a0f77e95e1195fbeeecf01e49739969
Shin-sibainu <shincode0712@gmail.com>
2023-05-25 17:37:40 +0900

    baseurl

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
-    // "Authorization": `Beare {token}`
   },
 });
 
